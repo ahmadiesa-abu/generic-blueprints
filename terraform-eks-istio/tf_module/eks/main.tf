02d3b63466bccce942b90ce2992c0100bc1c7d14
Ahmad Musa <ahmad@cloudify.co>
2022-09-08 21:34:05 +0300

    adjust-security-group-rules

--- terraform-eks-istio/tf_module/eks/main.tf
+++ terraform-eks-istio/tf_module/eks/main.tf
@@ -1,88 +1,3 @@
-# Ports needed to correctly install Istio for the error message: transport: Error while dialing dial tcp xx.xx.xx.xx15012: i/o timeout
-locals {
-  istio_ports = [
-    {
-      description = "Envoy admin port / outbound"
-      from_port   = 15000
-      to_port     = 15001
-    },
-    {
-      description = "Debug port"
-      from_port   = 15004
-      to_port     = 15004
-    },
-    {
-      description = "Envoy inbound"
-      from_port   = 15006
-      to_port     = 15006
-    },
-    {
-      description = "HBONE mTLS tunnel port / secure networks XDS and CA services (Plaintext)"
-      from_port   = 15008
-      to_port     = 15010
-    },
-    {
-      description = "XDS and CA services (TLS and mTLS)"
-      from_port   = 15012
-      to_port     = 15012
-    },
-    {
-      description = "Control plane monitoring"
-      from_port   = 15014
-      to_port     = 15014
-    },
-    {
-      description = "Webhook container port, forwarded from 443"
-      from_port   = 15017
-      to_port     = 15017
-    },
-    {
-      description = "Merged Prometheus telemetry from Istio agent, Envoy, and application, Health checks"
-      from_port   = 15020
-      to_port     = 15021
-    },
-    {
-      description = "DNS port"
-      from_port   = 15053
-      to_port     = 15053
-    },
-    {
-      description = "Envoy Prometheus telemetry"
-      from_port   = 15090
-      to_port     = 15090
-    },
-    {
-      description = "aws-load-balancer-controller"
-      from_port   = 9443
-      to_port     = 9443
-    }
-  ]
-
-  ingress_rules = {
-    for ikey, ivalue in local.istio_ports :
-    "${ikey}_ingress" => {
-      description = ivalue.description
-      protocol    = "tcp"
-      from_port   = ivalue.from_port
-      to_port     = ivalue.to_port
-      type        = "ingress"
-      self        = true
-    }
-  }
-
-  egress_rules = {
-    for ekey, evalue in local.istio_ports :
-    "${ekey}_egress" => {
-      description = evalue.description
-      protocol    = "tcp"
-      from_port   = evalue.from_port
-      to_port     = evalue.to_port
-      type        = "egress"
-      self        = true
-    }
-  }
-}
-
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
@@ -122,26 +37,57 @@ module "eks" {
     }
   }
 
+  cluster_security_group_additional_rules = {
+    egress_source_cluster_all = {
+      description                = "All Ports/protocols"
+      protocol                   = "-1"
+      from_port                  = -1
+      to_port                    = -1
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
 
-  # IMPORTANT
-  node_security_group_additional_rules = merge(
-    local.ingress_rules,
-    local.egress_rules
-  )
 
+  node_security_group_ntp_ipv4_cidr_block = ["169.254.169.123/32"]
+  node_security_group_additional_rules = {
+    ingress_self_nodes_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+  }
 }
 
-# Port needed to solve the error
-# Internal error occurred: failed calling
-# webhook "namespace.sidecar-injector.istio.io": failed to
-# call webhook: Post "https://istiod.istio-system.svc:443/inject?timeout=10s": # context deadline exceeded
-resource "aws_security_group_rule" "allow_sidecar_injection" {
-  description = "Webhook container port, From Control Plane"
-  protocol    = "tcp"
+resource "aws_security_group_rule" "allow_ingress_all_traffic" {
+  description = "Node to node all ports/protocols"
+  protocol    = "-1"
+  from_port   = 0
+  to_port     = 0
   type        = "ingress"
-  from_port   = 15017
-  to_port     = 15017
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  prefix_list_ids = []
+  self = null
+  source_security_group_id = null
 
   security_group_id        = module.eks.node_security_group_id
-  source_security_group_id = module.eks.cluster_primary_security_group_id
+}
+
+resource "aws_security_group_rule" "allow_egress_all_traffic" {
+  description = "Node to node all ports/protocols"
+  protocol    = "-1"
+  from_port   = 0
+  to_port     = 0
+  type        = "egress"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  prefix_list_ids = []
+  self = null
+  source_security_group_id = null
+
+  security_group_id        = module.eks.node_security_group_id
 }
